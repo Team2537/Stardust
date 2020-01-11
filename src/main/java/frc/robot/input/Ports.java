2fd2f74ec7bafa00524a5df5dc67676ab261b3f3
Daniel O <email2daniel2@gmail.com>
2020-01-11 12:13:09 -0500

    Sample Code for package structure on Dev!

---
+++ src/main/java/frc/robot/input/Ports.java
@@ -0,0 +1,7 @@
+package frc.robot.input;
+
+public class Ports {
+    public static int SERVO_PORT = 0;   // PWM port for servo motor
+    public static int OPEN_BUTTON = 1;  // A button opens mechanism
+    public static int CLOSE_BUTTON = 2; // B button closes mechanism
+  }
