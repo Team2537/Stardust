764ed9a8b0a4cc38e8a482da9b4d2bee990dd580
Luke Kadingo <lukeedingo@gmail.com>
2020-01-16 07:06:26 -0500

    Made a shooter subsytem

--- src/main/java/frc/robot/input/Ports.java
+++ src/main/java/frc/robot/input/Ports.java
@@ -4,4 +4,5 @@ public class Ports {
     public static int SERVO_PORT = 0;   // PWM port for servo motor
     public static int OPEN_BUTTON = 1;  // A button opens mechanism
     public static int CLOSE_BUTTON = 2; // B button closes mechanism
+    public static final int MOTOR_SHOOTER_PORT = 1; //Motor
   }
