aba8366f9d4e68f0100f314ca7cef0a18851fe11
Daniel O <email2daniel2@gmail.com>
2020-01-26 10:58:04 -0500

    fixed error in CANTalon

--- src/main/java/frc/robot/util/CANTalon.java
+++ src/main/java/frc/robot/util/CANTalon.java
@@ -7,8 +7,27 @@
 
 package frc.robot.util;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+
+
+
 /**
  * Add your docs here.
  */
-public class CANTalon {
+public class CANTalon extends TalonSRX{
+    private ControlMode controlMode = ControlMode.PercentOutput;
+
+    public CANTalon(int deviceNumber){
+        super(deviceNumber);
+    }
+
+    public void setControlMode(ControlMode mode){
+        this.controlMode = mode;
+    }
+
+    public void set(double demand){
+        this.set(controlMode, demand);
+    }
 }
