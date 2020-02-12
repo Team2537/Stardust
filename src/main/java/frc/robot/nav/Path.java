ce56e7768618d9691fddf9928cf3986248e15c64
Space RAIDers <email2daniel2@gmail.com>
2020-02-11 20:24:31 -0500

    fixed path

--- src/main/java/frc/robot/nav/Path.java
+++ src/main/java/frc/robot/nav/Path.java
@@ -1,13 +1,14 @@
 package frc.robot.nav;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class Path extends SequentialCommandGroup {
 
     public Path() {
-        addCommands(
-            new MecanumDriveStraightCommand(30, "X")
-        );
+        addCommands(new MecanumDriveStraightCommand(30, "X"));
     }
+
 
 }
