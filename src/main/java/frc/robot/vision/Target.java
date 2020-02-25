6ce99a50342a3b707428da1ff294d0c94280370d
Daniel O <email2daniel2@gmail.com>
2020-02-25 18:40:30 -0500

    added VisionTurnCommand()

--- src/main/java/frc/robot/vision/Target.java
+++ src/main/java/frc/robot/vision/Target.java
@@ -3,6 +3,8 @@ package frc.robot.vision;
 public class Target {
 	private Point[] points;
 	private Point[] boundingBox;
+	private Point boundingBoxCenter;
+	private double boundingBoxArea;
 
 	public Target(Point[] points) {
 		this.points = points;
@@ -39,5 +41,65 @@ public class Target {
 			boundingBox[1] = new Point(rightX, downY);
 		}
 		return boundingBox;
+	}
+
+	public double getBoundingBoxLength(){
+		if(boundingBox == null){
+			getBoundingBox();
+		}
+		return boundingBox[1].getX() - boundingBox[0].getX();
+	}
+	
+	public double getBoundingBoxHeight(){
+		if(boundingBox == null){
+			getBoundingBox();
+		}
+		return boundingBox[1].getY() - boundingBox[0].getY();
+	}
+	
+	public double getBoundingBoxArea(){
+		if(boundingBoxArea == 0){
+			boundingBoxArea = Math.abs(getBoundingBoxLength()*getBoundingBoxHeight());
+		}
+		return boundingBoxArea;
+	}
+	
+	public Point getBoundingBoxCenter(){
+		if(boundingBoxCenter == null){
+			Point topLeft = getBoundingBox()[0];
+			boundingBoxCenter = new Point((int)(topLeft.getX() + getBoundingBoxLength()/2),
+					(int)(topLeft.getY() + getBoundingBoxHeight()/2));
+		}
+		return boundingBoxCenter;
+	}
+
+	public static Point getMidpoint(Target[] targets){
+		if(targets.length < 2) return new Point(0,0);
+
+		double[] boundingBoxAreas = new double[targets.length];
+
+		for(int i = 0; i < boundingBoxAreas.length; i++){
+			System.out.println(boundingBoxAreas[i]);
+			boundingBoxAreas[i] = targets[i].getBoundingBoxArea();   
+		}
+
+		double first = -Double.MAX_VALUE, second = -Double.MAX_VALUE;
+		int firstIndex = 0, secondIndex = 0;
+		for (int i = 0; i < boundingBoxAreas.length; i++) {
+			if (boundingBoxAreas[i] > first) {
+				second = first;
+				first = boundingBoxAreas[i];
+				firstIndex = i;
+			}
+			// if current element is between first and second, update second to store value of current variable
+			else if (boundingBoxAreas[i] > second && boundingBoxAreas[i] != first){
+				second = boundingBoxAreas[i];
+				secondIndex = i;
+			}
+		}
+
+		Point box1 = targets[firstIndex].getBoundingBoxCenter();
+		Point box2 = targets[secondIndex].getBoundingBoxCenter();
+		return new Point((box1.getX() + box2.getX())/2, (box1.getY() + box2.getY())/2);
 	}
 }
